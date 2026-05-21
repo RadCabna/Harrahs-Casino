adf56463d8f6d583f74c9f0eeffba903e81ebd77
Radcabna <radcabna@gmail.com>
2026-05-21 15:37:21 +0300

    Initial Commit

---
+++ Harrahs Casino/Harrahs_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harrahs_CasinoApp.swift
+//  Harrahs Casino
+//
+//  Created by Алкександр Степанов on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Harrahs_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
